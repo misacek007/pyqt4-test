bb1e5aac6d15af776921f64944bdf128c70adac9
Michal Novacek <mnovacek@redhat.com>
2014-04-16 15:32:48 +0200

    initial commit of all files

---
+++ mainwindow.py
@@ -0,0 +1,22 @@
+from PyQt4 import uic
+
+( Ui_MainWindow, QMainWindow ) = uic.loadUiType( 'mainwindow.ui' )
+
+class MainWindow ( QMainWindow ):
+    """MainWindow inherits QMainWindow"""
+
+    def __init__ (self, parent = None):
+        QMainWindow.__init__( self, parent )
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi( self )
+        self.connect_slots()
+
+    def __del__ (self):
+        self.ui = None
+        
+    def connect_slots(self):
+        self.ui.pushButton.clicked.connect(self.pushButton_slot)
+        print self.ui.pushButton.clicked.connect
+    
+    def pushButton_slot(self):
+        print "pushButton_slot called"
